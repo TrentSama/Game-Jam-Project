a3e59280d2509a5b5450cd7152bf67f00b943406
TrentSama <trentdog2@gmail.com>
2018-07-16 02:27:02 -0500

    Added enemySpawner and enemy
    While the enemies work like they should, despawning when you get far enough away, the enemy spawner is acting up, and it'll take a while to get it sorted

---
+++ Game Jam ProtoType/Assets/EnemySpawner.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawner : MonoBehaviour {
+    public GameObject enemyType;
+    public Transform spawnLocation;
+
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            Instantiate(enemyType, spawnLocation.transform);
+        }
+    }
+}
