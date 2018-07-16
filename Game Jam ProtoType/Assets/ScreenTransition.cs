e00b9df1f899f68873974f4be45f11cf9bce3aac
TrentSama <trentdog2@gmail.com>
2018-07-15 21:17:30 -0500

    Set up camera switching mechanic

---
+++ Game Jam ProtoType/Assets/ScreenTransition.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Cinemachine;
+
+public class ScreenTransition : MonoBehaviour {
+    public CinemachineVirtualCamera mainCam;
+    public CinemachineVirtualCamera[] sideCams;
+    public PlayerController playerController;
+
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            GameObject player = GameObject.FindGameObjectWithTag("Player");
+            PlayerController playerController = player.GetComponent<PlayerController>();
+            for (int i = 0; i < sideCams.Length; i++)
+            {
+                sideCams[i].gameObject.SetActive(false);
+            }
+            mainCam.gameObject.SetActive(true);
+            StartCoroutine(TransitionPause());
+        }
+    }
+
+    IEnumerator TransitionPause()
+    {
+        playerController.canMove = false;
+        yield return new WaitForSeconds(1);
+        playerController.canMove = true;
+    }
+
+}
--- Game Jam ProtoType/Assets/Scripts/PlayerController.cs
+++ Game Jam ProtoType/Assets/Scripts/PlayerController.cs
@@ -3,23 +3,27 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour {
-
+    
     public Rigidbody2D rb;
     public float speed;
-    public float thrust; 
+    public float thrust;
+    public int playerCamera;
+    public bool canMove = true;
 
-	// Use this for initialization
-	void Start () {
+    // Use this for initialization
+    void Start () {
+        playerCamera = 10;
         rb = GetComponent<Rigidbody2D>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-        float h = Input.GetAxisRaw("Horizontal");
-        float v = Input.GetAxisRaw("Vertical");
 
-        Vector2 movement = new Vector2(h, v);
-        rb.velocity = (movement * speed);
+
+        if (canMove == true)
+        {
+            Moving();
+        }
 
         if (Input.GetButtonDown("Fire1"))
         {
@@ -27,5 +31,22 @@ public class PlayerController : MonoBehaviour {
         }
 
 	}
+
+    public void Moving()
+    {
+        float h = Input.GetAxisRaw("Horizontal");
+        float v = Input.GetAxisRaw("Vertical");
+
+        Vector2 movement = new Vector2(h, v);
+        rb.velocity = (movement * speed);
+
+    }
+
+    IEnumerator TransitionPause()
+    {
+        canMove = false;
+        yield return new WaitForSeconds(1);
+        canMove = true;
+    }
 
 }
