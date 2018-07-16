78132d2a035b4f7109b3929c6026725dcdc40f2d
TrentSama <trentdog2@gmail.com>
2018-07-15 19:01:53 -0500

    The project file

---
+++ Game Jam ProtoType/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    public Rigidbody2D rb;
+    public float speed;
+    public float thrust; 
+
+	// Use this for initialization
+	void Start () {
+        rb = GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float h = Input.GetAxisRaw("Horizontal");
+        float v = Input.GetAxisRaw("Vertical");
+
+        Vector2 movement = new Vector2(h, v);
+        rb.velocity = (movement * speed);
+
+        if (Input.GetButtonDown("Fire1"))
+        {
+            rb.AddForce(transform.up * thrust, ForceMode2D.Impulse);
+        }
+
+	}
+
+}
