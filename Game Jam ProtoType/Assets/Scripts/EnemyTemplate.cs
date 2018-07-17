58bdd0cefdfeb37f39b690a29de767890db1154d
TrentSama <trentdog2@gmail.com>
2018-07-16 22:25:38 -0500

    Enemies now follow player
    Also fixed spawn locations so they work now

---
+++ Game Jam ProtoType/Assets/Scripts/EnemyTemplate.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyTemplate : MonoBehaviour {
+    private int enemyStartingHealth = 100;
+    public int enemyCurrentHealth;
+    public int enemyDamage;
+    public float timeBetweenAttack;
+    public Collider2D despawnRadius;
+    public GameObject enemy;
+    private Transform target;
+    public float speed;
+    public float chaseRange;
+
+	// Use this for initialization
+	void Start () {
+        // Initializing the health of the enemy
+        enemyCurrentHealth = enemyStartingHealth;
+        GameObject player = GameObject.FindGameObjectWithTag("Player");
+        PlayerController playerController = player.GetComponent<PlayerController>();
+        target = GameObject.Find("Player").transform;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        float distanceToTarget = Vector2.Distance(transform.position, target.position);
+        if (distanceToTarget < chaseRange)
+        {           
+           transform.position = Vector2.MoveTowards(transform.position, target.position, speed * Time.deltaTime);           
+        }
+	}
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            Object.Destroy(enemy);
+        }
+    }
+}
