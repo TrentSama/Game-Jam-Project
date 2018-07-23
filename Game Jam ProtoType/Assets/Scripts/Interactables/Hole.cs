a86d9918797e166a02c8331898e0824f9011ee41
Isaac <isacmen@gmail.com>
2018-07-22 18:37:15 -0700

    new bg, hole

---
+++ Game Jam ProtoType/Assets/Scripts/Interactables/Hole.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hole : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnTriggerStay2D (Collider2D col) {
+		if (col.CompareTag("Pushable") && SamePosition(col.transform)){
+			Destroy (col.gameObject);
+		}
+	}
+
+	private bool SamePosition (Transform trans) {
+		if (trans.position.y <= this.transform.position.y + 0.025f
+			&& trans.position.y >= this.transform.position.y - 0.025f
+			&& trans.position.x <= this.transform.position.x + 0.025f
+			&& trans.position.x >= this.transform.position.x - 0.025f) {
+			return true;
+		}
+		return false;
+	}
+}
