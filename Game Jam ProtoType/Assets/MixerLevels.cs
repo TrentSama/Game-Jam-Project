70d7b913d22777b2283c033560dc11236428a87d
TrentSama <trentdog2@gmail.com>
2018-07-25 17:05:51 -0500

    Pause menu is fully functional!
    Will have to start setting audio sources to the mixer now by the way

---
+++ Game Jam ProtoType/Assets/MixerLevels.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class MixerLevels : MonoBehaviour {
+
+    public AudioMixer masterMixer;
+
+    public void SetSfxLvl(float sfxLvl)
+    {
+        masterMixer.SetFloat("effectsVol", sfxLvl);
+    }
+
+    public void SetMusicLvl(float musicLvl)
+    {
+        masterMixer.SetFloat("musicVol", musicLvl);
+    }
+}
---
+++ Game Jam ProtoType/Assets/OptionsMenu.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OptionsMenu : MonoBehaviour {
+    Canvas optionsCanvas;
+
+    private void Start()
+    {
+        optionsCanvas = GetComponent<Canvas>();
+    }
+
+    public void OptionsPanel()
+    {
+        optionsCanvas.enabled = !optionsCanvas.enabled;
+    }
+
+}
