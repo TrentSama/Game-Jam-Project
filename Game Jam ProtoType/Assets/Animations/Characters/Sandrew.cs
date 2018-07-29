c60fa4030974e77c752e53a2a9d233ffb5143bb6
Isaac <isacmen@gmail.com>
2018-07-28 23:39:20 -0700

    overworld mostly done

---
+++ Game Jam ProtoType/Assets/Animations/Characters/Sandrew.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Sandrew : MonoBehaviour {
+
+	public Puke puke;
+	public Sprite dead;
+	private Animator anim;
+
+	// Use this for initialization
+	void Start () {
+		anim = GetComponent<Animator> ();
+	}
+
+	void OnTriggerEnter2D (Collider2D col) {
+		if (col.CompareTag ("PlayerAttack")) {
+			this.GetComponent<SpriteRenderer> ().sprite = dead;
+			this.GetComponent<DialogTrigger> ().enabled = false;
+			puke.satisfied = true;
+
+		}
+	}
+}
---
+++ Game Jam ProtoType/Assets/Scripts/FishBoi.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FishBoi : MonoBehaviour {
+
+	public DialogTrigger olddialog;
+	public DialogTrigger newdialog;
+	public GameObject body;
+	public int toll;
+	private PlayerManager pm;
+	private bool rich;
+
+	// Use this for initialization
+	void Start () {
+		pm = FindObjectOfType<PlayerManager> ();
+		rich = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (pm.money > toll) {
+			rich = true;
+			olddialog.enabled = false;
+			newdialog.enabled = true;
+		}
+	}
+		
+	void OnTriggerEnter2D (Collider2D col) {
+		if (rich && Input.GetButtonDown ("Fire1")) {
+			body.SetActive (false);
+		}
+	}
+
+}
