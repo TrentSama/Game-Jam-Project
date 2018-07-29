47d5466209fc60e8810222226cbc2a85afd180ab
TrentSama <trentdog2@gmail.com>
2018-07-28 23:27:33 -0500

    More stuff i dunno

---
+++ Game Jam ProtoType/Assets/CrabSounds.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CrabSounds : MonoBehaviour {
+    public AudioSource pincers;
+    public AudioClip[] snipSounds;
+	// Use this for initialization
+	
+    void PincerSounds()
+    {
+        int ran = Random.Range(0, snipSounds.Length);
+        pincers.clip = snipSounds[ran];
+        pincers.Play();
+    }
+
+}
--- Game Jam ProtoType/Assets/Scripts/UI/OptionsMenu.cs
+++ Game Jam ProtoType/Assets/Scripts/UI/OptionsMenu.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class OptionsMenu : MonoBehaviour {
     Canvas optionsCanvas;
+    public Canvas pauseCanvas;
 
     private void Start()
     {
@@ -12,6 +13,7 @@ public class OptionsMenu : MonoBehaviour {
 
     public void OptionsPanel()
     {
+        pauseCanvas.enabled = !pauseCanvas.enabled;
         optionsCanvas.enabled = !optionsCanvas.enabled;
     }
 
